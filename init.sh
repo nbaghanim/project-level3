a8bb257c3552ed8a0f5f43eb21f0de4fa170572a
nbaghanim <nbaghanim@gmail.com>
2020-10-24 22:09:56 +0300

    add init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git clone https://github.com/KnowledgeHut-AWS/k8s-sandbox
+git clone https://github.com/nbaghanim/k8s-sandbox
 cd k8s-sandbox
 make cluster-up init install-cicd install-secrets
                          --from-literal=storageClassName=manual \
                          -o yaml -n tekton-pipelines \
-                         --dri-run=client | kubectl replace -f -
-
+                         --dry-run=client | kubectl replace -f -
+kubectl apply -f secret.yaml -n tekton-pipelines
+kubectl apply -f config-map.yaml -n tekton-pipelines
